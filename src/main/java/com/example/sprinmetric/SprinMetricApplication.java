8e9e3d498d70a492fca5316cecbad0986b53a812
ArturArtimonte <artur.artimonte@gmail.com>
2023-08-07 17:09:36 -0300

    first commit

---
+++ src/main/java/com/example/sprinmetric/SprinMetricApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprinmetric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinMetricApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinMetricApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sprinmetric/SprinMetricApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprinmetric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinMetricApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
